02eee407ac39fbb85416a0adb47c1387ad4e956d
Tarc <Tarc@B98>
2018-10-30 16:00:11 +0800

    ChongJun BOSS

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("IPhoneX o0o");
+        
         
     }
     
